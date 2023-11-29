e936b563aea69e94d8529b48a70e5b9832d5616b
kyled103 <128111583+kyled103@users.noreply.github.com>
2023-11-28 18:36:16 -0800

    Updated Pin Names

--- SerialMonitorData.h
+++ SerialMonitorData.h
@@ -1,25 +1,25 @@
 enum PIN
 {
-	D0 = 1,
-	D1,
-	D2,
-	D3,
-	D4,
-	D5,
-	D6,
-	D7,
-	D8,
-	D9,
-	D10,
-	D11,
-	D12,
-	D13,
-	A0,
-	A1,
-	A2,
-	A3,
-	A4,
-	A5,
+	p_D0 = 1,
+	p_D1,
+	p_D2,
+	p_D3,
+	p_D4,
+	p_D5,
+	p_D6,
+	p_D7,
+	p_D8,
+	p_D9,
+	p_D10,
+	p_D11,
+	p_D12,
+	p_D13,
+	p_A0,
+	p_A1,
+	p_A2,
+	p_A3,
+	p_A4,
+	p_A5,
 };
 
 #ifdef ARDUINO
@@ -37,84 +37,84 @@ short ParseData(byte data)
 
 	switch (data)
 	{
-		case D0:
+		case p_D0:
 			value = digitalRead(0);
 			break;
 
-		case D1:
+		case p_D1:
 			value = digitalRead(1);
 			break;
 
-		case D2:
+		case p_D2:
 			value = digitalRead(2);
 			break;
 
-		case D3:
+		case p_D3:
 			value = digitalRead(3);
 			break;
 
-		case D4:
+		case p_D4:
 			value = digitalRead(4);
 			break;
 
-		case D5:
+		case p_D5:
 			value = digitalRead(5);
 			break;
 
-		case D6:
+		case p_D6:
 			value = digitalRead(6);
 			break;
 
-		case D7:
+		case p_D7:
 			value = digitalRead(7);
 			break;
 
-		case D8:
+		case p_D8:
 			value = digitalRead(8);
 			break;
 
-		case D9:
+		case p_D9:
 			value = digitalRead(9);
 			break;
 
-		case D10:
+		case p_D10:
 			value = digitalRead(10);
 			break;
 
-		case D11:
+		case p_D11:
 			value = digitalRead(11);
 			break;
 
-		case D12:
+		case p_D12:
 			value = digitalRead(12);
 			break;
 
-		case D13:
+		case p_D13:
 			value = digitalRead(13);
 			break;
 
-		case A0:
-			value = analogRead(0);
+		case p_A0:
+			value = analogRead(PIN_A0);
 			break;
 
-		case A1:
-			value = analogRead(1);
+		case p_A1:
+			value = analogRead(PIN_A1);
 			break;
 
-		case A2:
-			value = analogRead(2);
+		case p_A2:
+			value = analogRead(PIN_A2);
 			break;
 
-		case A3:
-			value = analogRead(3);
+		case p_A3:
+			value = analogRead(PIN_A3);
 			break;
 
-		case A4:
-			value = analogRead(4);
+		case p_A4:
+			value = analogRead(PIN_A4);
 			break;
 
-		case A5:
-			value = analogRead(5);
+		case p_A5:
+			value = analogRead(PIN_A5);
 			break;
 
 		default:
